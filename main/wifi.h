1408bb695f3012d8e48dfadb3cdd4883feeda668
SxLiuYu <95198625+SxLiuYu@users.noreply.github.com>
2026-06-14 10:26:17 +0800

    feat: declare wifi_connect_sta(ssid, pass) in wifi.h

--- main/wifi.h
+++ main/wifi.h
@@ -3,6 +3,7 @@
 #include <esp_err.h>
 
 esp_err_t wifi_init(void);
+bool wifi_connect_sta(const char *ssid, const char *pass);
 bool wifi_connect_with_smartconfig(void);
 bool wifi_is_connected(void);
 void wifi_register_button_callback(void (*cb)(void*), void *arg);
